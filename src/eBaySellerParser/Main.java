7f3bcd8158c24136262c40c4812532bda0d18c66
asemenov <semenov@goodwix.ru>
2015-02-25 17:00:06 +0300

    change

--- src/eBaySellerParser/Main.java
+++ src/eBaySellerParser/Main.java
@@ -3,7 +3,14 @@ package eBaySellerParser;
 
 import com.ebay.sdk.ApiContext;
 import com.ebay.sdk.ApiCredential;
+import com.ebay.sdk.TimeFilter;
 import com.ebay.sdk.call.GetSellerListCall;
+import com.ebay.soap.eBLBaseComponents.DetailLevelCodeType;
+import com.ebay.soap.eBLBaseComponents.ItemType;
+import com.ebay.soap.eBLBaseComponents.PaginationType;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 //token
@@ -18,8 +25,34 @@ public class Main {
         ApiCredential cred = apiContext.getApiCredential();
 
         String token = "AgAAAA**AQAAAA**aAAAAA**R1TsVA**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6AGkIuoDJWHowidj6x9nY+seQ**lrgCAA**AAMAAA**wYDPchozhcBVlC/p7t3EUlf1emmWhWHpyUtBjLyGfW1J+wapCELriiK0WpkBQ8kVMISqr70JWGajFRZcBxYvMVSXGt0ZRvsOMS6EFQVo75M67PAW7Eq494HejyIxBmEX+/JO6xIwxbw1vh2ETa7pjcSdIOJZYp4/K2wmV5ZcuqfFfsstPcKcsnJpNBbPZIqxH+1LttvqIZSZcGF2zuIXuDfS55ee6laKNeRb/7bvMg941adyip2ROZGQq+98SnYjwzFKHA8ZennwYrgGriMKiTQVexxn26zp3WXNgcuD36LVYCVEZCuXCcQgAZuuWT7Iija2tm+flE7UjDlaOqmPPADNxysgNsOGaQztvxT+ncHGFZK3bIUx7+vhC1DNLNvpF76FRG0Y7WjpToq/cttdgAQYpu/Ngs3XylKLlOwg6IEHu0+4LXLJtqrkGVQ1G1bja2YRkqLq87CHvcOFW391vNHxHUoBx+5YWK674mbAVs2+XH8EQgcn9MAoypboS3TOdAMAGrzF0WOCLtddguhqaADttecKT2nwMWMgXmlFBr5Tmbe2pUn/N8Md8K8nFIpqk/ys1kkkKKigu+s3GtFWnZIOAGrZZrkyB5YBKBVUdHl3L7qNgQ9Vunbr6Dk0A11ItJ4lFWecj4imgCYb6xQtAlpnjIATpcYTPIR/BXfNA4v5sllmwQ0r6ycwLlPpxNruq+YMFcYXkWBpnUFtLBTFHW0rO2qgz9PUBMfGn1Yn2xozQb3TycfRpvOX02dA9QLG";
+        cred.seteBayToken(token);
+        apiContext.setApiServerUrl("https://api.ebay.com/wsapi");
+        GetSellerListCall getSellerListCall = new GetSellerListCall(apiContext);
+        getSellerListCall.setUserID("mem-store");
 
-        cred.seteBayToken("");
+        GregorianCalendar time1 = new GregorianCalendar(2014, Calendar.SEPTEMBER,5);
+        GregorianCalendar time2 = new GregorianCalendar(2015, Calendar.JANUARY,1);
+        TimeFilter fromTime = new TimeFilter(time1, time2);
 
+        GregorianCalendar time3 = new GregorianCalendar(2010, Calendar.JANUARY,1);
+        GregorianCalendar time4 = new GregorianCalendar(2015, Calendar.JANUARY,1);
+        TimeFilter toTime = new TimeFilter(time3, time4);
+
+
+        DetailLevelCodeType[] details = {DetailLevelCodeType.RETURN_ALL};
+        getSellerListCall.setDetailLevel(details);
+        PaginationType paginationType = new PaginationType();
+        paginationType.setEntriesPerPage(200);
+        paginationType.setPageNumber(1);
+        getSellerListCall.setPagination(paginationType);
+
+        getSellerListCall.setStartTimeFilter(fromTime);
+        //getSellerListCall.setEndTimeFilter(toTime);
+        try {
+            ItemType[] items =  getSellerListCall.getSellerList();
+            int a = 0;
+        } catch (Exception e){
+            System.out.print(e.toString());
+        }
     }
 }
